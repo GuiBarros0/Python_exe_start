3787f24bc82b67cf33c1700d952e2df24cd4a97c
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2021-10-28 00:39:26 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,4 +0,0 @@
-med1=float(input('informe o valor do primeiro medicamento: '))
-med2=float(input('informe o valor do segundo medicamento: '))
-valor= med1 + med2
-print('o valor gasto na sua compra é de R$',valor)
