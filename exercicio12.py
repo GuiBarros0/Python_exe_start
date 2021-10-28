177e0d15231d988135b9461661ea9ff348ba7778
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2021-10-28 00:39:10 -0300

    Delete exercicio12.py

--- exercicio12.py
+++
@@ -1,6 +0,0 @@
-nome=input('informe o nome do funcionario: ')
-salariobase=float(input('informe o salario base do funcionario R$'))
-imposto=salariobase*3/100
-gratis=salariobase*5/100
-salarioliquido = salariobase + gratis - imposto
-print('seu salario final desse mês é de R$',salarioliquido,)
