9d21823db99d70c23914bceb1546840cbd93a0a1
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2021-10-28 00:39:42 -0300

    Delete exercicio6.py

--- exercicio6.py
+++
@@ -1,7 +0,0 @@
-nomevendedor=input('cadastre o nome do vendedor: ')
-preco=float(input('informe o preço da peça: '))
-quantidade=int(input('digita quatidade de peças: '))
-total=preco*quantidade
-comissao=total*5/100
-print('a comissão do funcionario: ',nomevendedor)
-print('é R$',comissao)
