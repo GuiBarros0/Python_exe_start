474a918d171647e2204637b2844be88a2c802a96
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2021-10-28 00:39:37 -0300

    Delete exercicio5.py

--- exercicio5.py
+++
@@ -1,6 +0,0 @@
-n1=float(input('insira o primeiro numero: '))
-n2=float(input('insira o segundo numero: '))
-quadrado1= n1 **2
-quadrado2= n2 **2
-soma=quadrado1 + quadrado2
-print('o valor do quadrado é: ',soma)
