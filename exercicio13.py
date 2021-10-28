dfbb03c7adf33d1cddf6ba2f5d45edc5bfa46c8a
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2021-10-28 00:39:15 -0300

    Delete exercicio13.py

--- exercicio13.py
+++
@@ -1,5 +0,0 @@
-deposito=float(input('informe o valor do deposito R$'))
-taxa=float(input('informe o valor do taxa R$'))
-rendimento=deposito*taxa/100
-total=deposito+rendimento
-print('seu deposito rendeu',rendimento, 'e seu valor final é de R$',total)
